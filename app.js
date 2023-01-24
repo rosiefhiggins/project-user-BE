5c6f9aec1c7ee9923c3f50bdc310b7c248cf41db
rosiefhiggins <rosie.higgins@kpmg.co.uk>
2023-01-24 12:24:49 +0000

    added get and post

--- app.js
+++ app.js
@@ -2,31 +2,43 @@ const express = require("express");
 const BodyParser=require("body-parser");
 const { MongoClient, ObjectId } = require('mongodb');
 const CONNECTION_URL = "mongodb+srv://theWasabiBees:theWasabiBees@thewasabibeesspike.n3qvyoo.mongodb.net/?retryWrites=true&w=majority";
-const DATABASE_NAME = "usernames";
+const DATABASE_NAME = "UserDatabase";
 
 const app = express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
-                    
+           
+
+app.get('/users', (req,res)=>{
+    collection.find({}).toArray((error,result)=>{
+        if(error){
+            return this.res.status(500).send(error)
+        }
+        res.send(result)
+    })
+})
+
 app.listen(9090, () => {
     MongoClient.connect(CONNECTION_URL, {useNewUrlParser: true}, (error, client)=>{
         if(error){
             throw error
         }
         database=client.db(DATABASE_NAME);
-        collection = database.collection("UserDatabase");
-        console.log("server is listening on port 9090...");
+        collection = database.collection("usernames");
+        console.log('listening on port 9090')
     })
 });
 
 
-app.get('/api/users', (req,res)=>{
-    collection.find({}).toArray((error,result)=>{
-        if(error){
-            return this.response.status(500).send(error)
+app.post("/users", (request, response) => {
+    collection.insertOne(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
         }
-        console.log(result)
-        this.response.send(result)
-    })
-})
+        response.send({msg: "user inserted"});
+    });
+});
+
+
+
 module.exports = app
